9df0b9bcbca36589eea486910b4b6e84c79ac1ff
Albert Pucio <acpucio@gmail.com>
2016-05-13 01:04:32 -0400

    Chapter 4 - Completed with solution

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -36,7 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rango'
 )
+
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
